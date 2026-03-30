7200f89fdb1f611e6f10e8d18e77683138db779b
felipeliralira <felipe.lira.oliveira@escola.pr.gov.br>
2026-03-30 11:50:17 -0300

    Add files via upload

---
+++ Atividade/atividade.py
@@ -0,0 +1,18 @@
+p1 = (2,3)
+p2 = (6,8)
+p3 = (1,4)
+
+linha = [p1,p2,p3]
+
+print(linha[1][1])
+
+print(tuple(linha))
+print(list(p1))
+
+palavra = "banana"
+lista = []
+
+for letra in palavra:
+    lista.append("_")
+
+print(lista)
--- Jogos/forca.py
+++ Jogos/forca.py
@@ -3,25 +3,45 @@ def jogar_forca():
     print("\nBem vindo ao Jogo da Forca\n")
     print("------------------------------")
 
-    lista = ["_ _ _ _ _ _ "]
+    arquivo = open("Jogos/palavras.txt", "r")
+    palavra = []
 
-    palavra = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    for linha in palavra:
+        linha = linha.strip()
+        palavra.append(linha)
+
+    arquivo.close()
+
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+    #for letra in palavra:
+        #letras_acertadas.append("_")
     perdeu = False
     acertou = False
+    erros = 0
     #enquanto nao acerta a palavra secreta 
     #o jogador não pode jogar
 
+    print("Você terá 6 tentativas. Boa sorte!")
+
     while (not perdeu and not acertou):
-        chute = remover_acentos(input("Digite uma letra: ")).strip()
+        chute = remover_acentos(input("Digite uma letra: "))
+        chute = chute.strip().upper()
         #index define a posição da letra na palavra
         index = 0
-        for letra in palavra:
-            if (chute.lower() == letra.lower()):
-                letras_acertadas[index] = letra
-            index = index + 1
-        print(letras_acertadas)
+        if(chute in palavra_secreta):
+            for letra in palavra_secreta:
+                if (chute.lower() == letra.lower()):
+                    letras_acertadas[index] = letra
+                index = index + 1
+        else:
+            erros = erros + 1
+            
+        perdeu = erros == 6
+        acertou = "_" not in letras_acertadas
 
+
+        print(letras_acertadas)
 
 #como resolver ocento nas palavras
 
